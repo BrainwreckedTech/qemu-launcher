5740dc48b42b426d0837f308b0a0ca14a3b5df4c
Paul A. Hinchberger III <paulhinchberger3@gmail.com>
2015-03-19 00:41:14 -0400

    Changed window title to 'QEMU Graphical Launcher'

--- qglgui.py
+++ qglgui.py
@@ -17,7 +17,7 @@ import wx.xrc
 class frmMain ( wx.Frame ):
 	
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"QEMU VM Launcher", pos = wx.DefaultPosition, size = wx.Size( 400,435 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"QEMU Graphical Launcher", pos = wx.DefaultPosition, size = wx.Size( 400,435 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 		
 		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
 		
